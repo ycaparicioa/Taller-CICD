999c084b206013a6471723b1776103c4a39f15ed
ycaparicioa <ycaparicioa@unal.edu.co>
2026-05-13 05:21:13 -0500

    otro

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.json({ message: 'Hola, soy camilo!' });
+    res.json({ message: 'Hola, DevOps!' });
 });
 
 app.get('/health', (req, res) => {
